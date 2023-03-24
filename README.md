34e4c43a1a45038bfacdedd0f9491bbd0baf6925
sredniak <50489621+sredniak@users.noreply.github.com>
2023-03-24 12:44:01 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Otwarte dane Gdańsk - Ważne bilety
+
+
+
+
+ Otwarte dane ZTM w Gdańsku - Ważne bilety[-> API](https://ckan.multimediagdansk.pl/dataset/tristar/resource/5552f5f2-1e3f-4aa4-9042-2853faccb418)
+
+
+
+
---
+++ index.js
@@ -0,0 +1,41 @@
+
+const taryfa = document.querySelector('.taryfa');
+const strefa = document.querySelector('.strefa');
+const date = document.querySelector('.date');
+const button = document.querySelector('button');
+const input = document.querySelector('input');
+const inputValue = document.querySelector('.input-value')
+
+const API_URL= 'https://ckan.multimediagdansk.pl/dataset/c24aa637-3619-4dc2-a171-a23eec8f2172/resource/5552f5f2-1e3f-4aa4-9042-2853faccb418/download/wazne_bilety.json'
+
+
+
+// const cardNumber = '1003909540';
+
+async function getData() {
+
+    const inputCardNumber = input.value
+    const response = await fetch(API_URL);
+    const data = await response.json();
+
+    
+    axios.get(API_URL).then(res => {
+    
+
+        inputValue.textContent = `Numer twojej karty: ${inputCardNumber}`
+
+        date.textContent = `Wazny do: ${res.data.karty[inputCardNumber].bilety[0].WaznyDo}`;
+        strefa.textContent = `Strefa: ${res.data.karty[inputCardNumber].bilety[0].Strefa}`;
+        taryfa.textContent = `Taryfa: ${res.data.karty[inputCardNumber].bilety[0].Taryfa}`;        
+
+        
+    })
+    
+}
+
+
+button.addEventListener('click', getData)
+
+
+//1959044354 
+//1569935300
